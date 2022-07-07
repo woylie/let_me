4d8a1ae6f7ec0681f059af2d89e20b59d8267dd4
Mathias Polligkeit <git@woylie.de>
2022-07-08 01:48:20 +0900

    improve documentation

--- lib/expel/policy.ex
+++ lib/expel/policy.ex
@@ -88,6 +88,24 @@ defmodule Expel.Policy do
     end
   end
 
+  @doc """
+  Defines an action that needs to be authorized.
+
+  Within the do-block, you can use the `allow/1`, `disallow/1` and `pre_hooks/1`
+  macros to define the checks to be run.
+
+  ## Example
+
+      action :article_create do
+        allow role: :admin
+        allow role: :writer
+      end
+
+      action :article_update do
+        allow role: :admin
+        allow [:own_resource, role: :writer]
+      end
+  """
   defmacro action(name, do: block) do
     quote do
       # reset attributes from previous `action/2` calls
@@ -107,18 +125,151 @@ defmodule Expel.Policy do
     end
   end
 
+  @doc """
+  Defines the checks to be run to determine if an action is allowed.
+
+  The argument can be:
+
+  - a function name as an atom
+  - a tuple with the function name and an additional argument
+  - a list of function names or function/argument tuples
+  - `true` - Always allows an action. This is useful in combination with the
+    `disallow/1` macro.
+
+  The function must be defined in the configured Checks module and take the
+  subject (current user), object as arguments, and if given, the additional
+  argument.
+
+  If a list is given as an argument, the checks are combined with a logical
+  `AND`.
+
+  If the `allow/1` macro is used multiple times within the same `action/2`
+  block, the checks of each macro call are combined with a logical `OR`.
+
+  ## Examples
+
+  Let's assume you defined the following checks:
+
+      defmodule MyApp.Policy.Checks do
+        def role(%User{role: role}, _, role), do: true
+        def role(_, _, _), do: false
+
+        def own_resource(%User{id: id}, %{user_id: id}, _), do: true
+        def own_resource(_, _, _), do: false
+      end
+
+  This would allow the `:article_update` action only if the current user has
+  the role `:admin`:
+
+      action :article_update do
+        allow role: :admin
+      end
+
+  This is equivalent to:
+
+      action :article_update do
+        allow {:role, :admin}
+      end
+
+  This would allow the `:article_update` action if the user has the role
+  `:writer` _and_ the article belongs to the user:
+
+      action :article_update do
+        allow [:own_resource, role: :writer]
+      end
+
+  This is equivalent to:
+
+      action :article_update do
+        allow [:own_resource, {:role, :writer}]
+      end
+
+  This would allow the `:article_update` action if
+  (the user has the role `:admin` _or_ (the user has the role `:writer` _and_
+  the article belongs to the user)):
+
+      action :article_update do
+        allow role: :admin
+        allow [:own_resource, role: :writer]
+      end
+  """
   defmacro allow(checks) do
     quote do
       Module.put_attribute(__MODULE__, :allow_checks, unquote(checks))
     end
   end
 
+  @doc """
+  Defines the checks to be run to determine if an action is disallowed.
+
+  If any of the checks evaluates to `true`, the `allow` checks are overridden
+  and the permission request is automatically denied.
+
+  If a list is given as an argument, the checks are combined with a logical
+  `AND`.
+
+  If the `allow/1` macro is used multiple times within the same `action/2`
+  block, the checks of each macro call are combined with a logical `OR`.
+
+
+  ## Examples
+
+  Let's assume you defined the following checks:
+
+      defmodule MyApp.Policy.Checks do
+        def role(%User{role: role}, _, role), do: true
+        def role(_, _, _), do: false
+
+        def own_resource(%User{id: id}, %{user_id: id}, _), do: true
+        def own_resource(_, _, _), do: false
+
+        def same_user(%User{id: id}, %User{id: id}, _), do: true
+        def same_user(_, _, _), do: false
+      end
+
+  This would allow the `:user_delete` by default, _unless_ the object is the
+  current user:
+
+      action :message do
+        allow true
+        disallow :same_user
+      end
+
+  This would allow the `:article_update` action only if the current user has
+  the role `:admin`, _unless_ the object is the current user:
+
+      action :user_delete do
+        allow role: :admin
+        disallow :same_user
+      end
+
+  This would allow the `:user_delete` by default, _unless_ the object is the
+  current user _and_ the current user is an admin:
+
+      action :message do
+        allow true
+        disallow [:same_user, role: :admin]
+      end
+
+  This would allow the `:user_delete` by default, _unless_ the object is the
+  current user _or_ the current user is a peasant:
+
+      action :user_delete do
+        allow true
+        disallow :same_user
+        disallow role: :peasant
+      end
+  """
   defmacro disallow(checks) do
     quote do
       Module.put_attribute(__MODULE__, :disallow_checks, unquote(checks))
     end
   end
 
+  @doc """
+  Registers one or multiple functions to run in order to hydrate the subject
+  and/or object of the request.
+  """
   defmacro pre_hooks(checks) do
     quote do
       Module.put_attribute(__MODULE__, :pre_hooks, unquote(checks))
