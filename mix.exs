de598a0f710dfa02e2807a28dde92e7891c2c1b3
Mathias Polligkeit <git@woylie.de>
2022-11-06 20:55:42 +0900

    update docs config

--- mix.exs
+++ mix.exs
@@ -67,8 +67,9 @@ defmodule LetMe.MixProject do
   defp docs do
     [
       main: "readme",
-      extras: ["README.md"],
-      source_ref: "main"
+      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: @version,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 end
