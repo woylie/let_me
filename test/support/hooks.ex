ad529dff5adfd449f51c4857f110e13d07606fb9
Stephen Hogan <stephen@willow.app>
2023-05-26 16:37:06 -0600

    add opts to authorize so that pre_hooks can build more complex subject/objects

--- test/support/hooks.ex
+++ test/support/hooks.ex
@@ -1,11 +1,17 @@
 defmodule LetMe.TestHooks do
   @moduledoc false
 
-  def preload_handsomeness(%{} = subject, %{} = object, factor: factor) do
-    {Map.put(subject, :handsomeness, subject.id * factor), object}
+  def preload_handsomeness(%{} = subject, %{} = object, opts) do
+    factor = Keyword.get(opts, :factor)
+    bonus = Keyword.get(opts, :bonus, 0)
+    {Map.put(subject, :handsomeness, subject.id * factor + bonus), object}
   end
 
   def preload_likeability(%{} = subject, %{} = object) do
     {Map.put(subject, :likeability, subject.id + 1), object}
+  end
+
+  def preload_likeability(%{} = subject, %{} = object, bonus: bonus) do
+    {Map.put(subject, :likeability, subject.id + bonus), object}
   end
 end
